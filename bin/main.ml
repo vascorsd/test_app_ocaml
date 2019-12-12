516e23d697a801ef264030231c77d272385d1c82
Vasco Dias <m+git@vascorsd.com>
2019-12-12 01:59:07 +0000

    Try to play with creating a list and printing it

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,8 @@
 open Lib
 
-let () = print_endline ("Hello, World! " ^ string_of_int magic_number)
+let some_list = 1 :: 2 :: 3 :: []
+
+let () = 
+  print_endline ("Hello, World! " ^ string_of_int magic_number);
+  print_newline ();
+  print_endline (String.concat ", " (List.map string_of_int some_list));
