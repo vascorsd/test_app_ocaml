02fddf6e945677520c11502b2e63a62309d071a9
Vasco Dias <m+git@vascorsd.com>
2019-12-15 00:34:27 +0000

    add .ocamlformat to use dune 2 format integration. format and promote files.

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,6 @@
 open Lib
 
-let () = 
+let () =
   print_endline ("Hello, World! " ^ string_of_int magic_number);
   print_newline ();
-  print_endline (String.concat ", " (List.map string_of_int some_list));
+  print_endline (String.concat ", " (List.map string_of_int some_list))
--- lib/lib.ml
+++ lib/lib.ml
@@ -2,10 +2,10 @@ let magic_number = 10
 
 let my_func x = x + 2
 
-let some_list = 1 :: 2 :: 3 :: []
+let some_list = [ 1; 2; 3 ]
 
 let empty_list = []
 
-let other_list = [1; 2; 3;]
+let other_list = [ 1; 2; 3 ]
 
 let tuple = (1, 2)
